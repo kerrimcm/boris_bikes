bda19844917a748accdaa455e78bc42d04b8c9e0
Kerri McMahon <kerrimcm25@gmail.com>
2021-03-30 18:36:36 +0100

    Progress end of day 1

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,15 @@
 require './lib/docking_station'
+require './lib/bike'
 
 describe DockingStation do 
-    it { is_expected.to respond_to :release_bike }
+  it { is_expected.to respond_to :release_bike }
+
+  # Your test should check that you can call working? on the result of DockingStation.release_bike, 
+  #and that the result of doing so is true.
+  
+  it 'returns true when we call release bike on docking station' do
+    docking_station = DockingStation.new
+    result = docking_station.release_bike
+    expect(result.working?).to be true
+  end 
 end
