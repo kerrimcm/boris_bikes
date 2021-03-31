f177fc25086619dca76b0e5aa58d1c1a215ce4e3
Kerri McMahon <kerrimcm25@gmail.com>
2021-03-31 18:45:30 +0100

    More updates end of Day 2

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,6 +2,10 @@ require 'docking_station'
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
+  
+  it 'releases working bikes' do
+    expect(Bike.new).to respond_to(:working?)
+  end
 
   describe '#release_bike' do
     it 'releases a bike' do
@@ -9,8 +13,6 @@ describe DockingStation do
       subject.dock(bike)
       expect(subject.release_bike).to eq(bike)
     end
-  end
-  describe '#release_bike' do
     it 'raises an error when there are no bikes available' do
       expect { subject.release_bike }.to raise_error 'No bikes available'
     end
